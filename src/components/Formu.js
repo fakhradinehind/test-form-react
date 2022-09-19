6aa8574cd7c678608b84fac62701a5097e603404
farahi ismail <ifarahi@192.168.1.3>
2022-09-19 12:38:06 +0100

    fix(app): fix the context provider

--- src/components/Formu.js
+++ src/components/Formu.js
@@ -1,58 +1,89 @@
-import React,{useState} from 'react'
-import {Consumer} from './context'
-import './form.css'
-const Formu=()=> {
-  const [state,setState]=useState({
-    name:'',
-    email:'',
-    password:''
-  }),
-  onChangeInput=(e)=>setState({
-    [e.target.name]:e.target.value
-  }),
-  submit=(e)=>{
-    e.preventDefault();
-    console.log("hello hind")
-  }
+import React, { useState } from "react";
+import { Consumer } from "./context";
+import "./form.css";
+const Formu = () => {
+  const [state, setState] = useState({
+      name: "",
+      email: "",
+      password: "",
+    }),
+    onChangeInput = (e) =>
+      setState({
+        [e.target.name]: e.target.value,
+      }),
+    submit = (e) => {
+      e.preventDefault();
+      console.log("hello hind");
+    };
 
-  
-  return(
+  return (
     <Consumer>
-      {value=>{
-             return (
-              <form onSubmit={submit}>
-              <div className="card">
-                <div className="card-body">
-                  <div className="from-group">
-                      <div className="form-control">
-                          <label htmlFor="name">name:</label>
-                          <input type="text" name="name" id="name" className="form-control" defaultValue={state.name} onChange={onChangeInput} placeholder="" aria-describedby="helpId  " />
-                          <small id="helpId" className="text-muted">your name</small>
-                      </div>
-                      <div className="form-control">
-                          <label htmlFor="email">email</label>
-                          <input type="text" name="email" id="email" className="form-control" defaultValue={state.email} onChange={onChangeInput} placeholder="" aria-describedby="helpId" />
-                          <small id="helpId" className="text-muted">your email</small>
-                      </div>
-                      <div className="form-control">
-                          <label htmlFor="password">Password</label>
-                          <input type="text" name="password" id="password" className="form-control" defaultValue={state.password} onChange={onChangeInput} placeholder="" aria-describedby="helpId" />
-                          <small id="helpId" className="text-muted">your password</small>
-                      </div>
-                      <br/>
-                      <div className="form-group ">
-                          <button className="btn btn-primary">valider</button>
-                      </div>
-                      
+      {(value) => {
+        console.log("value:", value);
+        return (
+          <form onSubmit={submit}>
+            <div className="card">
+              <div className="card-body">
+                <div className="from-group">
+                  <div className="form-control">
+                    <label htmlFor="name">name:</label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="form-control"
+                      defaultValue={state.name}
+                      onChange={onChangeInput}
+                      placeholder=""
+                      aria-describedby="helpId  "
+                    />
+                    <small id="helpId" className="text-muted">
+                      your name
+                    </small>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      className="form-control"
+                      defaultValue={state.email}
+                      onChange={onChangeInput}
+                      placeholder=""
+                      aria-describedby="helpId"
+                    />
+                    <small id="helpId" className="text-muted">
+                      your email
+                    </small>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="text"
+                      name="password"
+                      id="password"
+                      className="form-control"
+                      defaultValue={state.password}
+                      onChange={onChangeInput}
+                      placeholder=""
+                      aria-describedby="helpId"
+                    />
+                    <small id="helpId" className="text-muted">
+                      your password
+                    </small>
+                  </div>
+                  <br />
+                  <div className="form-group ">
+                    <button className="btn btn-primary">valider</button>
                   </div>
                 </div>
               </div>
-            </form>
-              
-            )
+            </div>
+          </form>
+        );
       }}
     </Consumer>
-    )
- 
-}
-export default Formu
+  );
+};
+export default Formu;
--- src/components/context.js
+++ src/components/context.js
@@ -1,32 +1,31 @@
-import React,{useState} from 'react'
-const Context=React.createContext()
-const reducer=(state,action)=>{
-    switch(action.type){
-        case 'ADD_CONTACT':
-        return{
-          contacts:[action.payload,...state.contacts]
-        };
-        default:
-          return state;
-    }
+import React, { useState } from "react";
+const Context = React.createContext();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_CONTACT":
+      return {
+        contacts: [action.payload, ...state.contacts],
+      };
+    default:
+      return state;
   }
- export const Provider=()=> {
-    const [state,setState]=({
-        contacts:[
-            {name:"hind1",email:"fakhrad@gmail.com",password:"hindhind"},
-            {name:"hind", email:"fakhrad@gmail.com",password:"hindhind"},
-            {name:"hind2",email:"fakhrad@gmail.com",password:"hindhind"},
-            {name:"hind3",email:"fakhrad@gmail.com",password:"hindhind"},
-            {name:"hind4",email:"fakhrad@gmail.com",password:"hindhind"},
-            {name:"hind5",email:"fakhrad@gmail.com",password:"hindhind"}
-        ],
-        dispatch:action=>this.setState(state=>reducer(state,action))
-    })
-  return (
-    <Context.Provider value={state}>
-            
-      </Context.Provider>
-  )
-}
+};
+export const Provider = ({ children }) => {
+  const [state, setState] = useState([
+    { name: "hind1", email: "fakhrad@gmail.com", password: "hindhind" },
+    { name: "hind", email: "fakhrad@gmail.com", password: "hindhind" },
+    { name: "hind2", email: "fakhrad@gmail.com", password: "hindhind" },
+    { name: "hind3", email: "fakhrad@gmail.com", password: "hindhind" },
+    { name: "hind4", email: "fakhrad@gmail.com", password: "hindhind" },
+    { name: "hind5", email: "fakhrad@gmail.com", password: "hindhind" },
+  ]);
 
-export const Consumer = Context.Consumer
+  const contextValue = {
+    contacts: state,
+    dispatch: (action) => setState((state) => reducer(state, action)),
+  };
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
+
+export const Consumer = Context.Consumer;
